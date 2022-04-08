58aa6a0715989091538076cb3487ae9cfc951aaf
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-04-09 01:14:28 +0300

    Flip Icon

--- app/src/main/java/com/metehanbolat/bottomnavigationcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/bottomnavigationcompose/MainActivity.kt
@@ -3,6 +3,28 @@ package com.metehanbolat.bottomnavigationcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.metehanbolat.bottomnavigationcompose.ui.theme.BottomNavigationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,8 +32,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BottomNavigationComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(
+                                        text = "Bottom Navigation",
+                                        modifier = Modifier.fillMaxWidth(),
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            )
+                        },
+                        content = { },
+                        bottomBar = {
 
+                        }
+                    )
+                }
             }
         }
     }
 }
+
+
+@Composable
+fun FlipIcon(
+    modifier: Modifier = Modifier,
+    isActive: Boolean,
+    activeIcon: ImageVector,
+    inactiveIcon: ImageVector,
+    contentDescription: String
+) {
+    val animationRotation by animateFloatAsState(
+        targetValue = if (isActive) 180f else 0f,
+        animationSpec = spring(
+            stiffness = Spring.StiffnessLow,
+            dampingRatio = Spring.DampingRatioMediumBouncy
+        )
+    )
+    Box(
+        modifier = modifier.graphicsLayer { rotationY = animationRotation },
+        contentAlignment = Center
+    ) {
+        Icon(
+            rememberVectorPainter(image = if (animationRotation > 90) activeIcon else inactiveIcon),
+            contentDescription = contentDescription
+        )
+    }
+}
+
+
